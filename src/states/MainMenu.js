11aacc7a59979093e68d2a57034328bb7ec5b547
ASanti <Darkstars31@gmail.com>
2017-11-29 22:26:47 -0600

    MainMenu updates

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -11,18 +11,45 @@ export default class extends Phaser.State {
     banner.padding.set(10, 16)
     banner.anchor.setTo(0.5)
 
-    let menuItems = ['New Game', 'Continue', 'Settings']
+    let menuItems = [{ label: 'New Game', inputEnabled: true}, 
+                     { label: 'Continue', inputEnabled: true}, 
+                     { label: 'Settings', inputEnabled: true}
+                ];
     let menuSpacing = 0;
-    menuItems.forEach( (item) => {
-        let banner = this.add.text(this.world.centerX, this.game.height / 3 + menuSpacing, item, {font: 'Bangers', fontSize: 20, fill: '#77BFA3', smoothed: false})
-        banner.padding.set(10, 16)
-        banner.anchor.setTo(0.5)  
+    menuItems.forEach( (menuItem) => {
+        let item = this.add.text(this.world.centerX, this.game.height / 3 + menuSpacing, menuItem.label, {font: 'Bangers', fontSize: 20, fill: '#77BFA3', smoothed: false})
+        item.padding.set(10, 16)
+        item.anchor.setTo(0.5)  
+        item.inputEnabled = menuItem.inputEnabled;
+        item.useHandCursor = true;
+        this.addInputs(item);
         menuSpacing += 32;
-    })
+    });
 
+  }
 
-   
+  addInputs(item) {
+    item.events.onInputUp.add(()=> {this.onMenuItemClick(item)});   
+    item.events.onInputOver.add(()=> {this.onMenuItemHover(item)});
+    item.events.onInputOut.add(()=> { console.log('Out')});
+  }
 
+  onMenuItemHover(item) {
+
+  }
+
+  onMenuItemClick(item) {
+    console.log(item);
+    switch(item){
+        case 'New Game':
+            this.state.start('Game'); 
+            break;
+        case 'Continue':
+            break;
+        case 'Settings':
+            break;
+        default: console.log("WTF, how did you even?"); break;         
+    }
   }
 
   render () {
