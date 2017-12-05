1652f4eb1bd1eeaab1a08665105a8a7777259d91
ASanti <Darkstars31@gmail.com>
2017-12-05 00:01:26 -0600

    backgrounds, turns kind of, damage to both, animations, nasty code

--- src/states/Game.js
+++ src/states/Game.js
@@ -9,7 +9,17 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-    this.add.sprite(0,0, 'battleBackground1');
+    let isPlayersTurn = true;
+    this.buttonList = [];
+
+    let background = this.add.sprite(0,0, 'battleBackground1');
+    background.scale.setTo(.68,.68);
+
+    let playerSprite = this.add.sprite(this.world.width /4, this.world.height / 2, 'noviceWalk');
+    playerSprite.scale.x = -1;
+    playerSprite.anchor.setTo(.5,.5);
+    playerSprite.animations.add('walk');
+    playerSprite.animations.play('walk', 10, true);
 
     let numClouds = this.rnd.integerInRange(1, 3);
     this.clouds = [];
@@ -32,11 +42,10 @@ export default class extends Phaser.State {
 
     this.initUserInterface();
 
-
     this.mushroom = new Mushroom({
       game: this.game,
-      x: this.world.centerX + 180,
-      y: this.world.centerY + 100,
+      x: this.world.width /4,
+      y: this.world.height / 2 - 45,
       asset: 'mushroom'
     })
 
@@ -44,11 +53,11 @@ export default class extends Phaser.State {
   }
 
   initUserInterface () {
-    this.playerHp = this.add.text(60, this.game.height / 12, this.player.hp + "/" + this.player.hpMax, {font: 'Patua One', fontSize: 16, fill: '#333333', smoothed: false})
+    this.playerHp = this.add.text(60, this.game.height / 12, this.player.hp + "/" + this.player.hpMax, {font: 'Patua One', fontSize: 16, fill: '#fff', smoothed: false})
     this.playerHp.padding.set(10, 16)
     this.playerHp.anchor.setTo(0.5)
 
-    this.mobHp = this.add.text( this.game.width - 60, this.game.height / 12, this.mob.hp + "/" + this.mob.hpMax, {font: 'Patua One', fontSize: 16, fill: '#333333', smoothed: false})
+    this.mobHp = this.add.text( this.game.width - 60, this.game.height / 12, this.mob.hp + "/" + this.mob.hpMax, {font: 'Patua One', fontSize: 16, fill: '#fff', smoothed: false})
     this.mobHp.padding.set(10, 16)
     this.mobHp.anchor.setTo(0.5)
 
@@ -58,11 +67,13 @@ export default class extends Phaser.State {
       button.anchor.setTo(.5,.5);
       button.scale.setTo(item.scale.x,item.scale.y);
       uiButtonSpacing += 50;
-    });
+      this.buttonList.push(button);
+    });   
     
   }
 
   onAttackClick() {
+    this.isPlayersTurn = false;
     let damage = this.player.calculateAttack();
     console.log('Attack: ' + damage);
     this.mob.hp -= damage;
@@ -81,10 +92,19 @@ export default class extends Phaser.State {
         cloud.x = this.rnd.integerInRange(-500,-160);
       }
     });
-    this.playerHp.setText(this.player.hp.toFixed() + "/" + this.player.hpMax);
-    this.mobHp.setText(this.mob.hp.toFixed() + "/" + this.mob.hpMax);
+
+    if(this.isPlayersTurn){
+      this.buttonList.forEach((item)=> item.inputEnable = true);
+    } else {      
+      this.buttonList.forEach((item)=> item.inputEnable = false);      
+      this.player.hp -= this.mob.strength + this.rnd.integerInRange(4,5);
+      this.isPlayersTurn = true;
+    }
+
+    this.playerHp.setText( (this.player.hp > 0 ? this.player.hp.toFixed(): 0) + "/" + this.player.hpMax);
+    this.mobHp.setText( (this.mob.hp > 0 ? this.mob.hp.toFixed() : 0) + "/" + this.mob.hpMax);
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 132, 132)
+      this.game.debug.spriteInfo(this.mushroom, 130, 10)
     }
   }
 }
